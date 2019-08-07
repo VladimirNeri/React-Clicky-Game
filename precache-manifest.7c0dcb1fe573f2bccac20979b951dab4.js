2ba7b94f02316e12342a903d1540125aa28e5b59
obscura101 <vlad.neri@gmail.com>
2019-08-06 23:14:27 -0700

    Updates

---
+++ precache-manifest.7c0dcb1fe573f2bccac20979b951dab4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4aefef4375e33760ce6dbafbd122d84",
+    "url": "/React-Clicky-Game/index.html"
+  },
+  {
+    "revision": "f1660c55b80923d9f9ee",
+    "url": "/React-Clicky-Game/static/css/main.254d3fb6.chunk.css"
+  },
+  {
+    "revision": "85a1d543af124641f09c",
+    "url": "/React-Clicky-Game/static/js/2.5cb12968.chunk.js"
+  },
+  {
+    "revision": "f1660c55b80923d9f9ee",
+    "url": "/React-Clicky-Game/static/js/main.d54ee661.chunk.js"
+  },
+  {
+    "revision": "339ef2b4a9d9b3bbcf8d",
+    "url": "/React-Clicky-Game/static/js/runtime~main.25e3a6ff.js"
+  }
+]);
